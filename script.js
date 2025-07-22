4e1db5b9ad9aa8ec70f0dadd2ba72118d15ca248
BrunoCamariniJunior <brunocamarinijr@hotmail.com>
2025-07-21 23:21:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -50,6 +50,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var versiculos = {
     "2timoteo467": "⁶ Eu já estou sendo derramado como uma oferta de bebida. Está próximo o tempo da minha partida. ⁷ Combati o bom combate, terminei a corrida, guardei a fé.",
 	"2timoteo22": "² E as coisas que me ouviu dizer na presença de muitas testemunhas, confie a homens fiéis que sejam também capazes de ensinar a outros.",
+	"1corintios61920": "19 Acaso não sabem que o corpo de vocês é santuário do Espírito Santo que habita em vocês, que lhes foi dado por Deus, e que vocês não são de si mesmos? 20 Vocês foram comprados por alto preço. Portanto, glorifiquem a Deus com o seu próprio corpo.",
+	"1timoteo35": "5 Pois, se alguém não sabe governar sua própria família, como poderá cuidar da igreja de Deus?",
+	"efesios416": "16 Dele todo o corpo, ajustado e unido pelo auxílio de todas as juntas, cresce e edifica-se a si mesmo em amor, na medida em que cada parte realiza a sua função.",
+	"colossenses219": "19 Trata-se de alguém que não está unido à Cabeça, a partir da qual todo o corpo, sustentado e unido por seus ligamentos e juntas, efetua o crescimento dado por Deus.",
   };
 
   var links = document.querySelectorAll(".hyperlinkstyle[data-ref]");
